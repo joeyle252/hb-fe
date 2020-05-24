f6ed99fde77d9741a292dc940ce2d4c4609ef505
Khanh <lekimhongkhanh@gmail.com>
2020-05-24 14:26:07 +0700

    feat: added customized maxinmum price range slider UI component

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,44 +1,33 @@
-
-import React from 'react';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
+import React from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import Slider from "@material-ui/core/Slider";
+import PriceMaxSlider from "../PriceMaxSlider/PriceMaxSlider";
 
 export default function Filters() {
-    return (
-        <>
+  const [value, setValue] = React.useState(30);
 
-            <FormControl component="fieldset">
-                <FormLabel component="legend">Star Rating</FormLabel>
-                <FormGroup aria-label="position" column>
-                    <FormControlLabel
-                        value="end"
-                        control={<Checkbox color="primary" />}
-                        label="End"
-                        labelPlacement="end"
-                    />
-                    <FormControlLabel
-                        value="end"
-                        control={<Checkbox color="primary" />}
-                        label="End"
-                        labelPlacement="end"
-                    />
-                    <FormControlLabel
-                        value="end"
-                        control={<Checkbox color="primary" />}
-                        label="End"
-                        labelPlacement="end"
-                    />
-                    <FormControlLabel
-                        value="end"
-                        control={<Checkbox color="primary" />}
-                        label="End"
-                        labelPlacement="end"
-                    />
-                </FormGroup>
-            </FormControl>
-        </>
-    )
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Star Rating</FormLabel>
+        <FormGroup aria-label="position" column>
+          <FormControlLabel value="end" control={<Checkbox color="primary" />} label="End" labelPlacement="end" />
+          <FormControlLabel value="end" control={<Checkbox color="primary" />} label="End" labelPlacement="end" />
+          <FormControlLabel value="end" control={<Checkbox color="primary" />} label="End" labelPlacement="end" />
+          <FormControlLabel value="end" control={<Checkbox color="primary" />} label="End" labelPlacement="end" />
+        </FormGroup>
+        <FormLabel component="legend">Maximum Budget</FormLabel>
+        <FormGroup aria-label="position" column>
+          <PriceMaxSlider />
+        </FormGroup>
+      </FormControl>
+    </>
+  );
 }
