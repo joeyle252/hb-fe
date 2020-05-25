e689ea64ee21192cca04ada7a2f068a61163432d
Khanh <lekimhongkhanh@gmail.com>
2020-05-25 20:27:06 +0700

    bugfix: fix bootstrap container global collisions

--- src/pages/hotelDetail/HotelDetail.js
+++ src/pages/hotelDetail/HotelDetail.js
@@ -1,61 +1,22 @@
 import React from "react";
-import { Carousel } from "react-bootstrap";
+import HotelImageCarousel from "../../components/HotelImageCarousel/HotelImageCarousel";
+
+import Navbar from "../../components/Navbar/Navbar";
 import "./HotelDetail.css";
 
 export default function HotelDetail() {
   return (
-    <div className="container">
-      <div className="navbar">navbar</div>
-      <div className="sidebar">sidebar</div>
-      <div className="content">
-        <div> content</div>
-        <div> content</div>
-        <div> content</div>
-        <div> content</div>
-        content{" "}
+    <div className="detail-container bs-container">
+      <div className="detail-navbar">
+        <Navbar />
+      </div>
+      <div className="detail-sidebar">sidebar</div>
+      <div className="detail-content">
+        <p> hotel name </p>
+        <p> address</p>
+        <p> Star rating </p>
+        <HotelImageCarousel />
       </div>
     </div>
   );
-}
-
-{
-  /* <div>
-            <p> hotel name </p> 
-            <p> address</p>
-            <p> Star rating </p>
-        <Carousel style={{height:"600px", width:"600px"}}>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://q-cf.bstatic.com/images/hotel/max1024x768/157/157627359.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://q-cf.bstatic.com/images/hotel/max1024x768/166/166504847.jpg"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://r-cf.bstatic.com/images/hotel/max1024x768/166/166515066.jpg"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel> */
 }
